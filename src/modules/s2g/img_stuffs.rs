018f5fd3f4ffe4e27b56b8e04f791e2f3c5bb2a9
khang <mkhangle20@gmail.com>
2024-06-04 10:10:56 -0400

    s2g: fix color indexing

--- src/modules/s2g/img_stuffs.rs
+++ src/modules/s2g/img_stuffs.rs
@@ -10,12 +10,9 @@ use rayon::prelude::*;
 
 use super::constants::MAX_GAME_TEXTURE_SIZE;
 
-type Palette = (
-    Vec<quantette::palette::rgb::Rgb<quantette::palette::encoding::Srgb, u8>>,
-    Vec<u8>,
-);
+type Palette = Vec<quantette::palette::rgb::Rgb<quantette::palette::encoding::Srgb, u8>>;
 
-fn quantize_to_8bit(img: RgbImage) -> eyre::Result<(RgbImage, Palette)> {
+fn quantize_to_8pp(img: RgbImage) -> eyre::Result<(RgbImage, Palette)> {
     let pipeline = ImagePipeline::try_from(&img)?
         .palette_size(255)
         .dither(false)
@@ -23,7 +20,7 @@ fn quantize_to_8bit(img: RgbImage) -> eyre::Result<(RgbImage, Palette)> {
         .quantize_method(QuantizeMethod::kmeans());
 
     let img = pipeline.clone().quantized_rgbimage_par();
-    let palette: Palette = pipeline.indexed_palette_par();
+    let palette: Palette = pipeline.palette_par();
 
     Ok((img, palette))
 }
@@ -53,7 +50,7 @@ fn png_to_bmp(img_path: &Path) -> eyre::Result<()> {
     let img: RgbImage = image::open(img_path)?.into_rgb8();
     let img: RgbImage = maybe_resize(img);
     let (width, height) = img.dimensions();
-    let (img, (palette_color, palette_index)) = quantize_to_8bit(img)?;
+    let (img, palette_color) = quantize_to_8pp(img)?;
 
     let mut out_img = OpenOptions::new()
         .create(true)
@@ -62,15 +59,22 @@ fn png_to_bmp(img_path: &Path) -> eyre::Result<()> {
         .open(img_path.with_extension("bmp"))?;
 
     let mut encoder = image::codecs::bmp::BmpEncoder::new(&mut out_img);
-    let palette_color_arr = palette_color.iter().map(|p| [p.red, p.green, p.blue]).collect::<Vec<[u8; 3]>>();
+
+    let palette_color_arr = palette_color
+        .iter()
+        .map(|p| [p.red, p.green, p.blue])
+        .collect::<Vec<[u8; 3]>>();
+
+    // converting 24bpp into 8bpp with the palette we have
     let img_bmp_8pp = img
         .chunks_exact(3)
         .map(|p| {
             // unwrap is guaranteed because img uses palette colors
-            let index_for_color = palette_color_arr.iter()
+            let index_for_color = palette_color_arr
+                .iter()
                 .position(|pp| *pp == [p[0], p[1], p[2]])
                 .unwrap();
-            palette_index[index_for_color] as u8
+            index_for_color as u8
         })
         .collect::<Vec<u8>>();
 
@@ -81,6 +85,7 @@ fn png_to_bmp(img_path: &Path) -> eyre::Result<()> {
         image::ExtendedColorType::L8,
         Some(&palette_color_arr),
     )?;
+
     out_img.flush()?;
 
     Ok(())
