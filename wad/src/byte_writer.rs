271497eadae70a32e670e2af7986207c16801801
khang <mkhangle20@gmail.com>
2024-06-09 18:22:58 -0400

    wad: add writer

---
+++ wad/src/byte_writer.rs
@@ -0,0 +1,68 @@
+//! From https://github.com/khanghugo/dem/blob/master/src/byte_writer.rs
+
+pub struct ByteWriter {
+    pub data: Vec<u8>,
+    offset: usize,
+}
+
+impl ByteWriter {
+    pub fn new() -> Self {
+        Self {
+            data: Vec::new(),
+            offset: 0,
+        }
+    }
+
+    fn offset(&mut self, offset: usize) {
+        self.offset += offset;
+    }
+
+    pub fn get_offset(&self) -> usize {
+        self.offset
+    }
+
+    pub fn append_u32(&mut self, i: u32) {
+        self.data.extend(i.to_le_bytes());
+        self.offset(4);
+    }
+
+    pub fn append_i32(&mut self, i: i32) {
+        self.data.extend(i.to_le_bytes());
+        self.offset(4);
+    }
+
+    pub fn append_u8(&mut self, i: u8) {
+        self.data.extend(i.to_le_bytes());
+        self.offset(1);
+    }
+
+    pub fn append_i8(&mut self, i: i8) {
+        self.data.extend(i.to_le_bytes());
+        self.offset(1);
+    }
+
+    pub fn append_i16(&mut self, i: i16) {
+        self.data.extend(i.to_le_bytes());
+        self.offset(2);
+    }
+
+    pub fn append_u8_slice(&mut self, i: &[u8]) {
+        self.data.extend_from_slice(i);
+        self.offset(i.len());
+    }
+
+    pub fn append_i8_slice(&mut self, i: &[i8]) {
+        for what in i {
+            self.append_u8(*what as u8);
+        }
+    }
+
+    pub fn replace(&mut self, start: usize, length: usize, slice: &[u8]) {
+        self.data[start..(length + start)].copy_from_slice(&slice[..length]);
+    }
+
+    pub fn replace_with_u32(&mut self, start: usize, val: u32) {
+        let bytes = val.to_le_bytes();
+        self.replace(start, 4, &bytes);
+    }
+}
--- wad/src/lib.rs
+++ wad/src/lib.rs
@@ -3,20 +3,25 @@
 //! Based of specification from this webpage: https://twhl.info/wiki/page/Specification%3A_WAD3
 use std::{
     ffi::OsStr,
-    fmt::{self, Write},
-    path::Path,
+    fmt::{self, Display, Write as FmtWrite},
+    fs::OpenOptions,
+    io::Write,
+    path::{Path, PathBuf},
 };
 
+use byte_writer::ByteWriter;
 use nom::{
     combinator::{fail, map},
     error::context,
     multi::count,
-    number::complete::{le_i16, le_i32, le_i8, le_u32},
+    number::complete::{le_i16, le_i32, le_i8, le_u32, le_u8},
     sequence::tuple,
     IResult as _IResult,
 };
 
 use eyre::eyre;
+
+mod byte_writer;
 
 type IResult<'a, T> = _IResult<&'a [u8], T>;
 
@@ -40,15 +45,33 @@ pub struct DirectoryEntry {
 }
 
 #[derive(Debug)]
-pub struct TextureName(Vec<i8>);
+pub struct TextureName(Vec<u8>);
 
 impl TextureName {
-    pub fn get_bytes(&self) -> &Vec<i8> {
+    pub fn get_bytes(&self) -> &Vec<u8> {
         &self.0
     }
 }
 
-impl fmt::Display for TextureName {
+#[derive(Debug)]
+pub struct Image(Vec<Vec<u8>>);
+
+impl Image {
+    pub fn get_bytes(&self) -> &Vec<Vec<u8>> {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct Palette(Vec<Vec<u8>>);
+
+impl Palette {
+    pub fn get_bytes(&self) -> &Vec<Vec<u8>> {
+        &self.0
+    }
+}
+
+impl Display for TextureName {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for c in &self.0 {
             // do not write null
@@ -56,7 +79,7 @@ impl fmt::Display for TextureName {
                 continue;
             }
 
-            f.write_char(*c as u8 as char)?;
+            f.write_char(*c as char)?;
         }
 
         Ok(())
@@ -68,16 +91,16 @@ pub struct Qpic {
     pub width: i32,
     pub height: i32,
     // [[u8; width]; height]
-    pub data: Vec<Vec<i8>>,
+    pub data: Image,
     pub colors_used: i16,
     // Vec<[u8; 3]>
-    pub palette: Vec<Vec<i8>>,
+    pub palette: Palette,
 }
 
 #[derive(Debug)]
 pub struct MipMap {
     // [[u8; width]; height]
-    pub data: Vec<Vec<i8>>,
+    pub data: Image,
 }
 
 #[derive(Debug)]
@@ -95,9 +118,10 @@ pub struct MipTex {
     pub mip_images: Vec<MipMap>,
     pub colors_used: i16,
     // Vec<[u8; 3]>
-    pub palette: Vec<Vec<i8>>,
+    pub palette: Palette,
 }
 
+// this is not how it looks in file
 #[derive(Debug)]
 pub struct Entry {
     pub directory_entry: DirectoryEntry,
@@ -129,6 +153,139 @@ impl Wad {
 
         Self::from(&bytes)
     }
+
+    pub fn write_to_file(&self, path: impl AsRef<Path> + Into<PathBuf>) -> eyre::Result<()> {
+        let bytes = self.write_to_bytes();
+
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(path)?;
+
+        file.write_all(&bytes)?;
+
+        file.flush()?;
+
+        Ok(())
+    }
+
+    pub fn write_to_bytes(&self) -> Vec<u8> {
+        let mut writer = ByteWriter::new();
+
+        // write header
+        let header = &self.header;
+
+        writer.append_i8_slice(&header.magic);
+        writer.append_i32(header.num_dirs);
+        // just a dummy offset at this point.
+        let dir_offset_index = writer.get_offset();
+        writer.append_i32(header.dir_offset);
+
+        // need to write file first then we will write directory entry later
+        // with known offsets relatively from the end of the header,
+        // we can point to the correct data later on in directory
+        let file_entries_offset_and_length = self
+            .entries
+            .iter()
+            .map(|entry| {
+                let file_entry = &entry.file_entry;
+                let miptex_header_length = 16 + 4 + 4 + 4 * 4;
+                let file_entry_offset = writer.get_offset();
+
+                // write file entry
+                match file_entry {
+                    FileEntry::Qpic(_) => unimplemented!(),
+                    FileEntry::MipTex(MipTex {
+                        texture_name,
+                        width,
+                        height,
+                        mip_offsets: _,
+                        mip_images,
+                        colors_used: _,
+                        palette,
+                    }) => {
+                        let texture_name_bytes = texture_name.get_bytes();
+                        writer.append_u8_slice(texture_name_bytes);
+                        writer.append_u8_slice(&vec![0u8; 16 - texture_name_bytes.len()]);
+
+                        writer.append_u32(*width);
+                        writer.append_u32(*height);
+
+                        // mip_offsets
+                        writer.append_u32(miptex_header_length);
+                        writer.append_u32(miptex_header_length + width * height);
+                        writer.append_u32(
+                            miptex_header_length + width * height + (width * height) / 4,
+                        );
+                        writer.append_u32(
+                            miptex_header_length
+                                + width * height
+                                + (width * height) / 4
+                                + (width * height) / 4 / 4,
+                        );
+
+                        // mip images
+                        for image in mip_images {
+                            for row in image.data.get_bytes() {
+                                writer.append_u8_slice(row);
+                            }
+                        }
+
+                        // colors_used
+                        writer.append_i16(256);
+
+                        for row in palette.get_bytes() {
+                            writer.append_u8_slice(row);
+                        }
+                    }
+                }
+
+                // apparently, WE BYTES NEED TO BE ALIGNED HERE
+                // this is not documented anywhere
+                let offset_bytes_needed = writer.get_offset() % 4;
+
+                for _ in 0..offset_bytes_needed {
+                    writer.append_u8(0);
+                }
+
+                (file_entry_offset, writer.get_offset() - file_entry_offset)
+            })
+            .collect::<Vec<(usize, usize)>>();
+
+        // done writing the images, now we have the definite offset for our directory entry
+        let directory_entry_offset = writer.get_offset();
+        writer.replace_with_u32(dir_offset_index, directory_entry_offset as u32);
+
+        self.entries
+            .iter()
+            .zip(file_entries_offset_and_length)
+            .for_each(|(entry, (offset, length))| {
+                let DirectoryEntry {
+                    entry_offset: _,
+                    disk_size: _,
+                    entry_size: _,
+                    file_type,
+                    compressed: _,
+                    padding: _,
+                    texture_name,
+                } = &entry.directory_entry;
+
+                // write directory entry in a contiguous memory run
+                writer.append_i32(offset as i32);
+                writer.append_i32(length as i32);
+                writer.append_i32(length as i32);
+                writer.append_i8(*file_type);
+                writer.append_i8(0); // not compressed
+                writer.append_i16(256); // hard coded number of colors
+
+                let texture_name_bytes = texture_name.get_bytes();
+                writer.append_u8_slice(texture_name_bytes);
+                writer.append_u8_slice(&vec![0u8; 16 - texture_name_bytes.len()])
+            });
+
+        writer.data
+    }
 }
 
 fn parse_header(i: &[u8]) -> IResult<Header> {
@@ -151,7 +308,7 @@ fn parse_directory_entry(i: &[u8]) -> IResult<DirectoryEntry> {
             le_i8,
             le_i8, // https://github.com/Ty-Matthews-VisualStudio/Wally/blob/a05d3a11ac69aa81725fc7d4c6497b0523e92657/Source/Wally/WADList.h#L36
             le_i16,
-            count(le_i8, 16),
+            count(le_u8, 16),
         )),
         |(entry_offset, disk_size, entry_size, file_type, compressed, padding, texture_name)| {
             DirectoryEntry {
@@ -169,18 +326,18 @@ fn parse_directory_entry(i: &[u8]) -> IResult<DirectoryEntry> {
 
 fn parse_qpic(i: &[u8]) -> IResult<Qpic> {
     let (i, (width, height)) = tuple((le_i32, le_i32))(i)?;
-    let (i, data) = count(count(le_i8, width as usize), height as usize)(i)?;
+    let (i, data) = count(count(le_u8, width as usize), height as usize)(i)?;
     let (i, colors_used) = le_i16(i)?;
-    let (i, palette) = count(count(le_i8, 3), colors_used as usize)(i)?;
+    let (i, palette) = count(count(le_u8, 3), colors_used as usize)(i)?;
 
     Ok((
         i,
         Qpic {
             width,
             height,
-            data,
+            data: Image(data),
             colors_used,
-            palette,
+            palette: Palette(palette),
         },
     ))
 }
@@ -188,27 +345,27 @@ fn parse_qpic(i: &[u8]) -> IResult<Qpic> {
 fn parse_miptex(i: &[u8]) -> IResult<MipTex> {
     let struct_start = i;
 
-    let (i, texture_name) = count(le_i8, 16)(i)?;
+    let (i, texture_name) = count(le_u8, 16)(i)?;
     let (i, (width, height)) = tuple((le_u32, le_u32))(i)?;
     let (i, mip_offsets) = count(le_u32, 4)(i)?;
 
     // offset relatively from where we start with the struct
-    let (_, miptex0) = count(count(le_i8, width as usize), height as usize)(
+    let (_, miptex0) = count(count(le_u8, width as usize), height as usize)(
         &struct_start[(mip_offsets[0] as usize)..],
     )?;
 
-    let (_, miptex1) = count(count(le_i8, width as usize / 2), height as usize / 2)(
+    let (_, miptex1) = count(count(le_u8, width as usize / 2), height as usize / 2)(
         &struct_start[(mip_offsets[1] as usize)..],
     )?;
 
     let (_, miptex2) = count(
-        count(le_i8, width as usize / 2 / 2),
+        count(le_u8, width as usize / 2 / 2),
         height as usize / 2 / 2,
     )(&struct_start[(mip_offsets[2] as usize)..])?;
 
     // we get the palette start from the end of 4th miptex
     let (palette_start, miptex3) = count(
-        count(le_i8, width as usize / 2 / 2 / 2),
+        count(le_u8, width as usize / 2 / 2 / 2),
         height as usize / 2 / 2 / 2,
     )(&struct_start[(mip_offsets[3] as usize)..])?;
 
@@ -216,7 +373,7 @@ fn parse_miptex(i: &[u8]) -> IResult<MipTex> {
     let (palette_start, colors_used) = le_i16(palette_start)?;
 
     // hard code it to be 256 just to be safe
-    let (_, palette) = count(count(le_i8, 3), 256)(palette_start)?;
+    let (_, palette) = count(count(le_u8, 3), 256)(palette_start)?;
 
     Ok((
         i, // i here is pretty useless
@@ -226,13 +383,21 @@ fn parse_miptex(i: &[u8]) -> IResult<MipTex> {
             height,
             mip_offsets,
             mip_images: vec![
-                MipMap { data: miptex0 },
-                MipMap { data: miptex1 },
-                MipMap { data: miptex2 },
-                MipMap { data: miptex3 },
+                MipMap {
+                    data: Image(miptex0),
+                },
+                MipMap {
+                    data: Image(miptex1),
+                },
+                MipMap {
+                    data: Image(miptex2),
+                },
+                MipMap {
+                    data: Image(miptex3),
+                },
             ],
             colors_used,
-            palette,
+            palette: Palette(palette),
         },
     ))
 }
@@ -409,5 +574,44 @@ mod test {
             // right: "emp_ball1"
             // assert_eq!(file.texture_name.to_string(), "emp_ball1");
         }
+    }
+
+    #[test]
+    fn parse_write() {
+        let wad = Wad::from_file("test/wad_test.wad");
+
+        assert!(wad.is_ok());
+
+        let wad = wad.unwrap();
+
+        let res = wad.write_to_file("test/out/wad_test_out.wad");
+
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn parse_write2() {
+        let wad = Wad::from_file("test/wad_test2.wad");
+
+        assert!(wad.is_ok());
+
+        let wad = wad.unwrap();
+
+        let res = wad.write_to_file("test/out/wad_test2_out.wad");
+
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn parse_write3() {
+        let wad = Wad::from_file("test/surf_cyberwave.wad");
+
+        assert!(wad.is_ok());
+
+        let wad = wad.unwrap();
+
+        let res = wad.write_to_file("test/out/surf_cyberwave_out.wad");
+
+        assert!(res.is_ok());
     }
 }
