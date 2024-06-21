51a3d1ab8bbac29b428f46379510f9938a5067cf
khang <mkhangle20@gmail.com>
2024-06-21 12:05:41 -0400

    more aggressive smd split

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,6 +1,8 @@
 pub static MAX_GOLDSRC_TEXTURE_SIZE: u32 = 512;
 
-pub static MAX_SMD_TRIANGLE: usize = 4080;
+// divided by 2 just to be safe
+// divided by 2 again because what the fuck
+pub static MAX_SMD_TRIANGLE: usize = 4080 / 2 / 2;
 
 pub static STUDIOMDL_ERROR_PATTERN: &str = "************ ERROR ************";
 pub static MAX_GOLDSRC_MODEL_TEXTURE_COUNT: u32 = 64;
