a575be5f6d152f6c57e6d554a71e210ff35d646d
khang <mkhangle20@gmail.com>
2024-07-03 19:53:10 -0400

    update NO_RENDeR_TEXTURE

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -5,11 +5,20 @@ pub static MAX_GOLDSRC_TEXTURE_SIZE: u32 = 512;
 pub static MAX_SMD_TRIANGLE: usize = 4080 / 2 / 2;
 
 pub static STUDIOMDL_ERROR_PATTERN: &str = "************ ERROR ************";
-pub static MAX_GOLDSRC_MODEL_TEXTURE_COUNT: u32 = 64;
+pub static MAX_GOLDSRC_MODEL_TEXTURE_COUNT: usize = 64;
 
 pub static PALETTE_PAD_COLOR: [u8; 3] = [0, 0, 0];
 pub static PALETTE_TRANSPARENT_COLOR: [u8; 3] = [0, 255, 0];
 
-pub static NO_RENDER_TEXTURE: &[&str] = &["NULL", "HINT", "AAATRIGGER", "SKIP", "sky"];
+pub static ORIGIN_TEXTURE: &str = "ORIGIN";
+pub static NO_RENDER_TEXTURE: &[&str] = &[
+    "NULL",
+    "HINT",
+    "AAATRIGGER",
+    "SKIP",
+    "sky",
+    ORIGIN_TEXTURE,
+    "CLIP",
+];
 
 pub static EPSILON: f64 = 0.000001;
