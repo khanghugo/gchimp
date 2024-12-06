84b9ebf3a39cfefd4b7da423b4270dc1024d90f0
khang <mkhangle20@gmail.com>
2024-12-06 15:17:08 -0500

    loop_wave: convert to mono, pcm16u and 22050hz before looping the wave

--- gchimp/src/modules/loop_wave.rs
+++ gchimp/src/modules/loop_wave.rs
@@ -44,7 +44,41 @@ pub fn loop_wave(wav_path: impl AsRef<Path> + Into<PathBuf>) -> eyre::Result<()>
     Ok(())
 }
 
-pub fn loop_wave_from_wave_bytes(mut bytes: Vec<u8>) -> eyre::Result<Vec<u8>> {
+fn make_pcm16u_mono_and_22050(bytes: Vec<u8>) -> eyre::Result<Vec<u8>> {
+    let Ok(mut wav) = wav_io::reader::Reader::from_vec(bytes) else {
+        return err!("cannot read .wav bytes");
+    };
+
+    let Ok(header) = wav.read_header() else {
+        return err!("cannot get .wav header");
+    };
+
+    let Ok(f32_samples) = wav.get_samples_f32() else {
+        return err!("cannot get .wav samples");
+    };
+
+    let f32_samples = match header.channels {
+        1 => f32_samples,
+        2 => wav_io::utils::stereo_to_mono(f32_samples),
+        rest => return err!(".wav has too many channels: {}", rest),
+    };
+
+    const BEST_SAMPLING_RATE: u32 = 22050;
+
+    let samples2 = wav_io::resample::linear(f32_samples, 1, header.sample_rate, BEST_SAMPLING_RATE);
+
+    let res_wav_header = wav_io::new_header(BEST_SAMPLING_RATE, 16, false, true);
+
+    let Ok(res_bytes) = wav_io::write_to_bytes(&res_wav_header, &samples2) else {
+        return err!("cannot write .wav");
+    };
+
+    Ok(res_bytes)
+}
+
+pub fn loop_wave_from_wave_bytes(bytes: Vec<u8>) -> eyre::Result<Vec<u8>> {
+    let mut bytes = make_pcm16u_mono_and_22050(bytes)?;
+
     let cue = CuePoint::from_sample_offset(1, 1);
     let cues = [cue];
 
@@ -64,6 +98,12 @@ mod test {
     #[test]
     fn run() {
         let path = PathBuf::from("/home/khang/gchimp/examples/loop_wave/bhit_flesh-1.wav");
+        loop_wave(path).unwrap();
+    }
+
+    #[test]
+    fn run_32bit() {
+        let path = PathBuf::from("/home/khang/gchimp/examples/loop_wave/birds_32bit.wav");
         loop_wave(path).unwrap();
     }
 }
