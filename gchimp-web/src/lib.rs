44eab8d2313c0a479546a501d0b3845eb7cbd77c
khang <mkhangle20@gmail.com>
2025-01-09 14:26:57 -0500

    fix www compile

--- gchimp-web/src/lib.rs
+++ gchimp-web/src/lib.rs
@@ -35,7 +35,8 @@ pub fn resmake(bsp_bytes: Vec<u8>, filename: &str) -> Result<String, JsValue> {
 
     let bsp_path = Path::new(filename);
 
-    match resmake_single_bsp(&bsp, bsp_path, None, &ResMakeOptions { wad_check: false }) {
+    // does not include default resource by default
+    match resmake_single_bsp(&bsp, bsp_path, None, &ResMakeOptions { wad_check: false, include_default_resource: false }) {
         Err(err) => Err(JsValue::from_str(err.to_string().as_str())),
         Ok(ok) => Ok(ok),
     }
