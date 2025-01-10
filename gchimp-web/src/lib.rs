74fec35185d0f505c40f2d4e654748408e27619f
khang <mkhangle20@gmail.com>
2025-01-10 17:47:00 -0500

    syntax

--- gchimp-web/src/lib.rs
+++ gchimp-web/src/lib.rs
@@ -35,16 +35,16 @@ pub fn resmake(bsp_bytes: Vec<u8>, filename: &str) -> Result<String, JsValue> {
 
     let bsp_path = Path::new(filename);
 
+    let options = ResMakeOptions {
+        res: true,
+        zip: false,
+        wad_check: false,
+        include_default_resource: false,
+        zip_ignore_missing: false,
+    };
+
     // does not include default resource by default
-    match resmake_single_bsp(
-        &bsp,
-        bsp_path,
-        None,
-        &ResMakeOptions {
-            wad_check: false,
-            include_default_resource: false,
-        },
-    ) {
+    match resmake_single_bsp(&bsp, bsp_path, None, &options) {
         Err(err) => Err(JsValue::from_str(err.to_string().as_str())),
         Ok(ok) => Ok(ok),
     }
