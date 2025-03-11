9d4661b124f3b264e86c5d849ce216b5d77ea755
khang <mkhangle20@gmail.com>
2025-03-11 14:03:25 -0400

    waddy: enable tga import

--- gchimp-native/src/gui/constants.rs
+++ gchimp-native/src/gui/constants.rs
@@ -1,4 +1,4 @@
 pub static PROGRAM_WIDTH: f32 = 408.;
 pub static PROGRAM_HEIGHT: f32 = 480.0;
 
-pub static IMAGE_FORMATS: &[&str] = &["png", "bmp", "jpeg", "jpg"];
+pub static IMAGE_FORMATS: &[&str] = &["png", "bmp", "jpeg", "jpg", "tga"];
