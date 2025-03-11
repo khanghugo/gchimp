6f50aa51780a9cac016bec4709ece7586f597d7d
khang <mkhangle20@gmail.com>
2025-03-11 14:20:34 -0400

    waddy: actually enabling tga

--- gchimp-native/src/gui/constants.rs
+++ gchimp-native/src/gui/constants.rs
@@ -1,4 +1,4 @@
 pub static PROGRAM_WIDTH: f32 = 408.;
 pub static PROGRAM_HEIGHT: f32 = 480.0;
 
-pub static IMAGE_FORMATS: &[&str] = &["png", "bmp", "jpeg", "jpg", "tga"];
+pub static IMAGE_FORMATS: &[&str] = &["png", "bmp", "jpeg", "jpg", "tga", "vtf"];
