9bc583a3253b3252ec0c3a1940c2bacadc1603c1
khang <mkhangle20@gmail.com>
2026-07-15 20:58:59 -0400

    add hdr format constants

--- gchimp-native/src/gui/constants.rs
+++ gchimp-native/src/gui/constants.rs
@@ -1,4 +1,5 @@
-pub static PROGRAM_WIDTH: f32 = 408.;
-pub static PROGRAM_HEIGHT: f32 = 480.0;
+pub const PROGRAM_WIDTH: f32 = 408.;
+pub const PROGRAM_HEIGHT: f32 = 480.0;
 
-pub static IMAGE_FORMATS: &[&str] = &["png", "bmp", "jpeg", "jpg", "tga", "vtf"];
+pub const IMAGE_FORMATS: &[&str] = &["png", "bmp", "jpeg", "jpg", "tga", "vtf"];
+pub const HDR_FORMATS: &[&str] = &["hdri", "hdr", "exr", "dds"];
